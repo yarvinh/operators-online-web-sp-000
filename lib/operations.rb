715ccf17f9f39d3e716f2827f54509ee874890f9
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-21 16:51:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,14 +11,13 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60
-		true
-	elsif speed < 40
-		true
-	elsif speed >= 40 && speed <= 60
-		false
-
-
-	end
+	 speed > 60 ?	true
+	# elsif speed < 40
+	# 	true
+	# elsif speed >= 40 && speed <= 60 ?
+	# 	false
+	#
+	#
+	# end
 
 end
