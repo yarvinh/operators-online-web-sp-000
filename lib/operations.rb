cfd25d15671da9722c52da1a341a900334f2a27b
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-21 16:47:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,13 +3,22 @@ if speed > 60
 	true
 elsif speed < 40
 	true
-else false
-
+else
+	false
 end
 end
 
 
 
 def not_safe?(speed)
+	if speed > 60
+		true
+	elsif speed < 40
+		true
+	elsif speed > 40 && speed < 60
+		false
+
+
+	end
 
 end
